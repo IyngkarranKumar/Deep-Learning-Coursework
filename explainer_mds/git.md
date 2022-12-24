1af355031e00da01e921785d5b48969f4c17175b
Iyngkarran Kumar <iyngkarrankumar@Iyngkarrans-MacBook-Air.local>
2022-12-24 11:36:48 +0000

    init

---
+++ explainer_mds/git.md
@@ -0,0 +1,3 @@
+# How does git work?
+
+https://www.freecodecamp.org/news/learn-the-basics-of-git-in-under-10-minutes-da548267cc91/
---
+++ utils.py
@@ -0,0 +1,33 @@
+
+import torch
+import os
+
+
+def cycle(iterable):
+    while True:
+        for x in iterable:
+            yield x
+
+def normalise(x):
+  return (x-x.min()) / (x.max()-x.min())
+
+def save(save_dict,name):
+  path = os.path.join(os.getcwd(),"models",name)
+  torch.save(save_dict,path)
+
+def load(path,model,optimiser):
+
+  checkpoint = torch.load(path)
+  model_state = model.load_state_dict(checkpoint["model_state_dict"])
+  optimiser_state = optimiser.load_state_dict(checkpoint["optimiser_state_dict"])
+  epoch = checkpoint["epoch"]
+  loss = checkpoint["loss"]
+
+  return model,optimiser,epoch,loss
+
+
+def KLDivLoss(mu1,sigma1,mu2=0,sigma2=1):
+  #code from https://github.com/robert-lieck/Learning-Embeddings-by-Simulating-Communication/blob/main/CommunicationAutoencoder/util.py
+  
+  loss = (0.5*torch.log(sigma2/sigma1) + ((sigma1**2 + (mu1-mu2)**2) / (2*sigma2**2)) - 0.5).mean()
+  return loss
